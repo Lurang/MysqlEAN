5bc2b8c039c895598d723d6d7089cc26f95de94e
Lurang <qorrhaapfhd@naver.com>
2020-01-02 09:39:07 +0900

    change post-form with reactive forms

--- angular-frontend/src/app/board-post-form/board-post-form.component.ts
+++ angular-frontend/src/app/board-post-form/board-post-form.component.ts
@@ -23,10 +23,6 @@ export class BoardPostFormComponent implements OnInit {
     private router: Router,
     private location: Location,
   ) {
-    this.postForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      body: ['', Validators.required],
-    });
     this.route.paramMap.subscribe(params => {
       this.boardId = params.get('boardId');  
       this.postId = params.get('postId');    
@@ -34,6 +30,11 @@ export class BoardPostFormComponent implements OnInit {
   };
 
   ngOnInit() {
+    this.postForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      body: ['', Validators.required],
+    });
+
     if (this.postId) {
       this.getData(this.boardId, this.postId);
     };
@@ -44,8 +45,8 @@ export class BoardPostFormComponent implements OnInit {
       .subscribe((data) => {
         this.postForm.title = data.postInfo.post_title;
         this.postForm.body = data.postInfo.post_body;
-        this.postForm.value.title = data.postInfo.post_title;
-        this.postForm.value.body = data.postInfo.post_body;
+        this.postForm.get('title').setValue(data.postInfo.post_title);
+        this.postForm.get('body').setValue(data.postInfo.post_body);
       });
   };
   onSubmit(data) {
